533eafbac68c2b46e895f70eb7bdaaad9b70e7ce
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2018-03-27 14:29:54 +0200

    index modified

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 	require_once 'parsers/NbaLogParser.php';
 
 	$parser = new NbaLogParser;
-	// Set directory with log files that should be parsed
-	$parser->setLogDir('/Users/ruud/ETI/Zend workbenches/Current/nba-log-parser/2018-03-14');
-	// Directory to which output files are written
-	$parser->setOutputDir('/Users/ruud/ETI/Zend workbenches/Current/nba-log-parser/output');
-	// Remove older data
-	$parser->run();
-
+	$parser
+		->setLogDir('/Users/ruud/ETI/Zend workbenches/Current/nba-log-parser/2018-03-14')
+		->setOutputDir('/Users/ruud/ETI/Zend workbenches/Current/nba-log-parser/output')
+		->run();
+	
+	echo 'Ready';
 	
